28eb24d5825037a07734a060ba70b00e894ae88d
Inchill <173772140@qq.com>
2021-11-25 00:49:17 +0800

    Update main.test.js

--- tests/unit/main.test.js
+++ tests/unit/main.test.js
@@ -1,8 +1,8 @@
 import phoneFilter from '../../client/filters/phoneFilter.ts';
 import { getCookie, setCookie, delCookie } from '../../client/user/config/cookie.ts';
 
-test('13688476450 should to be 136****6450', () => {
-    expect(phoneFilter(13688476450)).toBe('136****6450');
+test('12312345678 should to be 123****5678', () => {
+    expect(phoneFilter(13688476450)).toBe('123****5678');
 });
 
 test('set cookie cid as name, 123 as value, 10s as expire', () => {
